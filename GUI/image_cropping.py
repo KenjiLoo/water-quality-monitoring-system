45c6dcb7e1e08c895dd22eeba2f9d52d03503404
KenjiLoo <80972386+KenjiLoo@users.noreply.github.com>
2022-04-28 16:17:50 +0800

    Update image_cropping.py
    Updated comments

--- GUI/image_cropping.py
+++ GUI/image_cropping.py
@@ -95,6 +95,7 @@ def main():
         """
         **Group defined function** \n
         This is a function that gets the coordinates of the box drawn by the user on the image.\n
+        :param window: a Tkinter Object 
         """
         global x_top, y_top, x_bot, y_bot
 
